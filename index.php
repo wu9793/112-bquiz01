a6e66ba260b8b1fdb9dec7b5fe30d45c953a7ee2
wu <timmy61501@gmail.com>
2023-12-04 14:03:43 +0800

    透過switch case 修改參數轉換主要頁面

--- index.php
+++ index.php
@@ -34,9 +34,27 @@
 						1 </span>
 				</div>
 			</div>
+
 			<!-- 引入 main -->
-			<?php include "./front/main.php";?>
+			<?php //include "./front/main.php";?>
 			<!-- 引入 main end -->
+
+			<?php
+			switch ($_GET['do']) {
+				case 'login':
+					include "./front/login.php";
+					break;
+				case 'main':
+					include "./front/main.php";
+					break;
+				case 'news':
+					include "./front/news.php";
+					break;
+			}
+
+
+			?>
+
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
 				$(".sswww").hover(
